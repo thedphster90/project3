c228a8e3a34334b801eb965df2d93c49d84960e7
thedphster90 <dphllrr@gmail.com>
2020-10-10 06:11:13 -0700

    switch to pc prob 7

--- venv/problem7.py
+++ venv/problem7.py
@@ -1,4 +1,7 @@
 
+import os
+
+
 # A RouteTrie will store our routes and their associated handlers
 class RouteTrie:
     def __init__(self):
@@ -27,13 +30,13 @@ class RouteTrie:
         for letter in pathlist:
 
             if letter not in pathlist:
-            current_node = current_node.children[letter]
+                current_node = current_node.children[letter]
         return current_node
 
         # Starting at the root, navigate the Trie to find a match for this path
         # Return the handler for a match, or None for no match
     pass
-# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
+# A  RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
 class RouteTrieNode:
     def __init__(self):
 
@@ -47,12 +50,60 @@ class RouteTrieNode:
 
 
 class Router:
-    def __init__(self):
+    def __init__(self, handler_init = ''):
+
+        self.router = RouteTrie()
+        self.router.root.handler = handler_init
+        self.handlerroot = self.router.root.handler
+
+    def add_handler(self, path, handler):
+
+        handlerlist = []
+
+        handlerlist += self.split_path(path)
+        node1 = self.router.insert(handlerlist, handler)
+
+        print(handlerlist)
 
 
 
+        # Add a handler for a path
+        # You will need to split the path and pass the pass parts
+        # as a list to the RouteTrie
+        pass
+
+    def lookup(self, path):
+
+        #m = self.router.find(path)
+        list1 = self.split_path(path)
+        print(list1)
+        m = self.router.find(list1)
+       # if self.router.root.handler == None:
+            #return None
+
+        if not m:
+            return print("handler not found")
+        else:
+            return print(m)
 
 
+
+    # lookup path (by parts) and return the associated handler
+    # you can return None if it's not found or
+    # return the "not found" handler if you added one
+    # bonus points if a path works with and without a trailing slash
+    # e.g. /about and /about/ both return the /about handler
+        pass
 
     def split_path(self, path):
         return [y for y in path.split("/") if y]
+
+
+
+newrouter = Router()
+newrouter.add_handler("www.hello.com/the/train/came/to/the/station")
+print(newrouter.lookup('the'))
+
+newrouter1 = Router()
+print(newrouter1.lookup('the'))
+newrouter1.lookup('hi')
